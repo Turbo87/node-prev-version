0d6376e1e7e9aec93e7f138e7c9443b9857aebdb
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-03-01 20:48:09 +0100

    Add prerelease filtering

--- index.js
+++ index.js
@@ -10,9 +10,17 @@ module.exports = function(version, versions) {
     throw new Error(`"${version}" is not found in the passed "versions" array.`);
   }
 
+  let isPreRelease = semver.prerelease(version) !== null;
+
   let previousVersions = versions
     .filter((_, _index) => _index < index)
-    .filter(version => semver.valid(version));
+    .filter(_version => semver.valid(_version))
+    .filter(_version => semver.gte(version, _version))
+    .filter(_version => {
+      let _isPreRelease = semver.prerelease(_version) !== null;
+      return isPreRelease || !_isPreRelease;
+    })
+    .sort(semver.compare);
 
   let previous = previousVersions.slice(-1)[0] || null;
   return { version, previous };
