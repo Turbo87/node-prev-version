5444e3a2ff73592991d14cd3c56a1ce72b8fbeae
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-03-01 20:28:23 +0100

    Add validity check

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const semver = require('semver');
+
 module.exports = function(version, versions) {
+  if (!semver.valid(version)) {
+    return { version, invalid: true };
+  }
+
   return {
     version,
   };
